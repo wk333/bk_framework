f7eaffc8420efd12cba1b4d71d1bfad17dadcbbe
wk333 <30464860+wk333@users.noreply.github.com>
2020-07-15 10:08:39 +0800

    Update default.py
    修改token

--- conf/default.py
+++ conf/default.py
@@ -31,9 +31,9 @@ from django.conf.global_settings import *  # noqa
 # ==============================================================================
 # 在蓝鲸智云开发者中心 -> 点击应用ID -> 基本信息 中获取 APP_ID 和 APP_TOKEN 的值
 APP_ID = 'bk1'
-APP_TOKEN = '85a953e4-d88d-48db-b52b-ce6e51b21140'
+APP_TOKEN = 'f256abac-4990-4d40-8861-5530489747e2'
 # 蓝鲸智云开发者中心的域名，形如：http://paas.bking.com
-BK_PAAS_HOST = 'http://wangkai-paas.yovole.com:84'
+BK_PAAS_HOST = 'https://paas-wangkai.bk.com'
 
 # 请求官方 API 默认版本号，可选值为："v2" 或 ""；其中，"v2"表示规范化API，""表示未规范化API
 DEFAULT_BK_API_VER = 'v2'
